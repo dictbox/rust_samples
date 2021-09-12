9272d48c298725e37fba3e0f7e3e425b1cf4cf23
wanlei <open@wanlei.net>
2021-09-12 19:53:49 +0800

    add sample 002

---
+++ 002_struct_demo/src/main.rs
@@ -0,0 +1,22 @@
+#[derive(Debug)]
+struct Rectangle{
+    width:u32,
+    height:u32,
+}
+
+impl Rectangle{
+    fn area(&self)->u32{
+        self.width*self.height
+    }
+}
+
+fn main() {
+    let rect = Rectangle{
+        width:30,
+        height:50,
+    };
+
+    println!("{}",rect.area());
+
+    println!("{:#?}",rect);
+}
